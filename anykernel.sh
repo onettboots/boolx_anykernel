d4a47866500d13afceda9deb8dbecb23dfbef34d
onettboots <blackcocopet@gmail.com>
2024-09-02 17:35:32 +0000

    fixups for Miui/HyperOS Veraion
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -9,10 +9,10 @@ do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
-do.cleanuponabort=1
+do.cleanuponabort=0
 device.name1=raphael
 device.name2=raphaelin
-supported.versions=
+supported.versions=10-15
 supported.patchlevels=
 '; } # end properties
 
@@ -45,38 +45,6 @@ case "$ZIPFILE" in
     ;;
     *)
     ui_print "";
-    ;;
-esac
-
-case "$ZIPFILE" in
-  *66fps*|*66hz*)
-    ui_print "  • Setting 66 Hz refresh rate"
-    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=1"
-    ;;
-  *69fps*|*69hz*)
-    ui_print "  • Setting 69 Hz refresh rate"
-    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=2"
-    ;;
-  *72fps*|*72hz*)
-    ui_print "  • Setting 72 Hz refresh rate"
-    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=3"
-    ;;
-  *75fps*|*75hz*)
-    ui_print "  • Setting 75 Hz refresh rate"
-    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=4"
-    ;;
-  *81fps*|*81hz*)
-    ui_print "  • Setting 81 Hz refresh rate"
-    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=5"
-    ;;    
-  *)
-    patch_cmdline "msm_drm.framerate_override" ""
-    fr=$(cat /sdcard/framerate_override | tr -cd "[0-9]");
-    [ $fr -eq 66 ] && ui_print "  • Setting 66 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=1"
-    [ $fr -eq 69 ] && ui_print "  • Setting 69 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=2"
-    [ $fr -eq 72 ] && ui_print "  • Setting 72 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=3"
-    [ $fr -eq 75 ] && ui_print "  • Setting 75 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=4"
-    [ $fr -eq 81 ] && ui_print "  • Setting 81 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=5"
     ;;
 esac
 
