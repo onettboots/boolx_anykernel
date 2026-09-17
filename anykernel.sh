323c41db3facc1008070c5794fbe1bab997a9876
onettboots <blackcocopet@gmail.com>
2026-09-17 15:28:15 +0700

    Boolx bringup
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=FloppyKernel v2.0b for Xiaomi Trinket devices by @Flopster101
+kernel.string=BoolxKernel for Xiaomi Trinket devices by @onettboots
 do.devicecheck=1
 do.modules=0
 do.systemless=1
